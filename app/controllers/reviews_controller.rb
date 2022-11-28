25420dc798d35e2c29b46ccc6736ef18e95b6a2a
Nana <nanaolomaiyani@gmail.com>
2022-11-28 21:28:21 +0300

    assignment done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,12 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
     render json: reviews, include: :dog_house
   end
 
@@ -10,14 +16,13 @@ class ReviewsController < ApplicationController
     review = Review.find(params[:id])
     render json: review, include: :dog_house
   end
-
+  
   def create
     review = Review.create(review_params)
     render json: review, status: :created
   end
 
   private
-
   def render_not_found_response
     render json: { error: "Review not found" }, status: :not_found
   end
